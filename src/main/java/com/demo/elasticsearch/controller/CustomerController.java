22e5b445087ab1cfe9c333da296cdb960a6f176b
samar <kssrgml@gmail.com>
2020-07-15 12:10:04 +0530

    test api in CustomerController

---
+++ src/main/java/com/demo/elasticsearch/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.demo.elasticsearch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@GetMapping("/test")
+	public String test() {
+		return "success";
+	}
+
+}
