360560f2db267a5e8a06ed8971a6aede30127b08
samar <kssrgml@gmail.com>
2020-07-15 15:10:46 +0530

    changes - dev

--- src/main/java/com/demo/elasticsearch/controller/CustomerController.java
+++ src/main/java/com/demo/elasticsearch/controller/CustomerController.java
@@ -14,7 +14,7 @@ public class CustomerController {
 		System.out.println("Hai this is Vipin");
 		System.out.println("Hai this is Sheo Om Shivasthav");
 		System.out.println("Hai this is Anirudh Joseph");
-		System.out.println("Hai this is Ranjitha Kulkarni - team lead");
+		System.out.println("Hai this is Sathu Bai - dev");
 
 		return "success";
 	}
